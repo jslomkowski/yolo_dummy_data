2d846bac1d1ed20d8b48bcca1afb180da4e703bb
Jerzy Slomkowski <jerzy.slomkowski@dsv.com>
2023-04-13 14:35:29 +0200

    [dev]

--- make_dummy_data.py
+++ make_dummy_data.py
@@ -1,143 +1,85 @@
-import random
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import shutil
+import random
 
-num_images = 1000
-img_size = 200
-max_box_size = img_size / 2
-min_box_size = img_size / 5
-max_num_boxes = 1
-min_num_boxes = 1
-train_ratio = 0.7
-test_ratio = 0.1
-val_ratio = 0.2
+image_shape = (200, 200)
+box_size = np.random.randint(10, 150)
+samples = 10
 
-try:
-    shutil.rmtree('datasets')
-except FileNotFoundError:
-    pass
+# Remove existing dataset directory, if present
+shutil.rmtree('datasets', ignore_errors=True)
 
 # Create directories to save images and labels
-os.makedirs('datasets/shapes/images/train', exist_ok=True)
-os.makedirs('datasets/shapes/images/test', exist_ok=True)
-os.makedirs('datasets/shapes/images/val', exist_ok=True)
-os.makedirs('datasets/shapes/labels/train', exist_ok=True)
-os.makedirs('datasets/shapes/labels/test', exist_ok=True)
-os.makedirs('datasets/shapes/labels/val', exist_ok=True)
+for dataset_type in ['train', 'test', 'val']:
+    for data_type in ['images', 'labels']:
+        os.makedirs(
+            f'datasets/shapes/{data_type}/{dataset_type}', exist_ok=True)
 
-# Calculate number of images for each set
-num_train_images = int(num_images * train_ratio)
-num_test_images = int(num_images * test_ratio)
-num_val_images = int(num_images * val_ratio)
 
-# Initialize counters for each set
-train_counter = 0
-test_counter = 0
-val_counter = 0
+# Generate images and labels
+for i in range(samples):
+    # Generate random shape and box size and position
+    shape_class = np.random.randint(0, 3)
+    box_position = np.random.randint(
+        0, image_shape[0] - box_size), np.random.randint(0, image_shape[1] - box_size)
 
-for i in range(num_images):
+    # Create figure and shape
+    fig, ax = plt.subplots(figsize=(6, 6))
+    if shape_class == 0:
+        shape = plt.Rectangle(box_position, box_size, box_size,
+                              linewidth=1, edgecolor='black', facecolor='black')
+    elif shape_class == 1:
+        shape = plt.Circle((box_position[0] + box_size / 2, box_position[1] + box_size / 2),
+                           box_size / 2, linewidth=1, edgecolor='black', facecolor='black')
+    else:
+        shape = plt.Polygon(np.array([(box_position[0] + box_size / 2, box_position[1]),
+                                      (box_position[0],
+                                       box_position[1] + box_size),
+                                      (box_position[0] + box_size, box_position[1] + box_size)]),
+                            linewidth=1, edgecolor='black', facecolor='black')
+    ax.add_patch(shape)
 
-    # i = 0
-    fig, ax = plt.subplots(figsize=(img_size / 100, img_size / 100))
-    ax.set_xlim([0, img_size])
-    ax.set_ylim([0, img_size])
-    ax.axis('off')  # Remove axis
-    num_boxes = random.randint(min_num_boxes, max_num_boxes)
-    shapes = ['rectangle', 'circle', 'triangle']
-    # shapes = ['rectangle']
+    # Set axis limits and remove axis
+    ax.set_xlim(0, image_shape[0])
+    ax.set_ylim(0, image_shape[1])
+    ax.invert_yaxis()
+    ax.set_axis_off()
 
-    boxes = []
-    for j in range(num_boxes):
-        shape = random.choice(shapes)
-        if shape == 'rectangle':
-            box_size = random.randint(min_box_size, max_box_size)
-            x1 = random.randint(0, img_size - box_size)
-            y1 = random.randint(0, img_size - box_size)
-            x2 = x1 + box_size
-            y2 = y1 + box_size
-            boxes.append((x1, y1, x2, y2))
-            rect = plt.Rectangle((x1, y1), box_size, box_size,
-                                 linewidth=1, edgecolor='black', facecolor='black')
-            ax.add_patch(rect)
-        elif shape == 'triangle':
-            size = random.randint(min_box_size, max_box_size)
-            x1 = random.randint(0, img_size - size)
-            y1 = random.randint(0, img_size - size)
-            x2 = x1 + size
-            y2 = y1
-            x3 = x1 + size // 2
-            y3 = y1 + size
-            boxes.append((x1 + size // 2, y1 + size // 2, size, size))
-            triangle = plt.Polygon(
-                [(x1, y1), (x2, y2), (x3, y3)], linewidth=1, edgecolor='black', facecolor='black')
-            ax.add_patch(triangle)
-        elif shape == 'circle':
-            radius = random.randint(min_box_size // 2, max_box_size // 2)
-            x_center = random.randint(radius, img_size - radius)
-            y_center = random.randint(radius, img_size - radius)
-            boxes.append((x_center, y_center, radius, radius))
-            circle = plt.Circle((x_center, y_center), radius,
-                                linewidth=1, edgecolor='black', facecolor='black')
-            ax.add_patch(circle)
-    # Save image to 'datasets/shapes/images/'
-    filename = f'random_image_{i}.png'
-    plt.savefig(f'datasets/shapes/images/{filename}')
+    # Save image and define YOLO labels
+    image_path = f'datasets/shapes/random_{i}.png'
+    plt.savefig(image_path, bbox_inches='tight', pad_inches=0, dpi=100)
     plt.close()
 
-    # Save box positions to a text file in YOLO format to 'datasets/shapes/labels/'
-    with open(f'datasets/shapes/labels/{os.path.splitext(filename)[0]}.txt', 'w') as f:
-        for box in boxes:
-            if shape == 'rectangle':
-                shape_class = 0
-            elif shape == 'circle':
-                shape_class = 1
-            elif shape == 'triangle':
-                shape_class = 2
+    class_name = str(shape_class)
+    x_center = (box_position[0] + box_size / 2) / image_shape[0]
+    y_center = (image_shape[1] - box_position[1] -
+                box_size / 2) / image_shape[1]
+    width = box_size / image_shape[0]
+    height = box_size / image_shape[1]
 
-            if shape == 'triangle':
-                x_center = box[0] / img_size
-                y_center = 1 - (box[1] / img_size)
-                width = size / img_size
-                height = size / img_size
-                f.write(
-                    f'{shape_class} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n')
-            elif shape == 'rectangle':
-                x_center = (box[0] + box[2]) / (2 * img_size)
-                y_center = 1 - ((box[1] + box[3]) / (2 * img_size))
-                width = (box[2] - box[0]) / img_size
-                height = (box[3] - box[1]) / img_size
-                f.write(
-                    f'{shape_class} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n')
-            elif shape == 'circle':
-                x_center = box[0] / img_size
-                y_center = 1 - (box[1] / img_size)
-                radius = radius * 2 / img_size
-                f.write(
-                    f'{shape_class} {x_center:.6f} {y_center:.6f} {radius:.6f} {radius:.6f}\n')
+    # Save YOLO labels to a text file
+    label_path = f'datasets/shapes/random_{i}.txt'
+    with open(label_path, 'w') as f:
+        f.write(
+            f"{class_name} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}")
 
-    # Split the dataset into train, test, and val
-    if train_counter < num_train_images:
-        # Save image to 'datasets/shapes/images/train/'
-        shutil.move(
-            f'datasets/shapes/images/{filename}', 'datasets/shapes/images/train/')
-        # Save label to 'datasets/shapes/labels/train/'
-        shutil.move(
-            f'datasets/shapes/labels/{os.path.splitext(filename)[0]}.txt', 'datasets/shapes/labels/train/')
-        train_counter += 1
-    elif test_counter < num_test_images:
-        # Save image to 'datasets/shapes/images/test/'
-        shutil.move(
-            f'datasets/shapes/images/{filename}', 'datasets/shapes/images/test/')
-        # Skip saving label for test set
-        os.remove(
-            f'datasets/shapes/labels/{os.path.splitext(filename)[0]}.txt')
-        test_counter += 1
-    elif val_counter < num_val_images:
-        # Save image to 'datasets/shapes/images/val/'
-        shutil.move(
-            f'datasets/shapes/images/{filename}', 'datasets/shapes/images/val/')
-        # Save label to 'datasets/shapes/labels/val/'
-        shutil.move(
-            f'datasets/shapes/labels/{os.path.splitext(filename)[0]}.txt', 'datasets/shapes/labels/val/')
-        val_counter += 1
+# Get a list of all the generated images and shuffle it
+images = [f'datasets/shapes/random_{i}.png' for i in range(samples)]
+random.shuffle(images)
+
+# Split the images into train, test, and validation sets
+train_size = int(len(images) * 0.8)
+test_size = int(len(images) * 0.1)
+
+train_images = images[:train_size]
+test_images = images[train_size:train_size + test_size]
+val_images = images[train_size + test_size:]
+
+# Move the images and labels to their respective directories
+for dataset_type, images in zip(['train', 'test', 'val'], [train_images, test_images, val_images]):
+    for image_path in images:
+        shutil.move(image_path, f'datasets/shapes/images/{dataset_type}')
+        label_path = image_path.replace('.png', '.txt')
+        shutil.move(label_path, f'datasets/shapes/labels/{dataset_type}')
